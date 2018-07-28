029154d2e60e781b2244be0c2262717822a4a884
Yuta Uchida <rjrhw977@yahoo.co.jp>
2018-07-28 17:59:19 +0900

    Add list item

--- src/App.js
+++ src/App.js
@@ -35,6 +35,10 @@ class App extends Component {
             <option value="2019">2019</option>
             <option value="2020">2020</option>
             <option value="2021">2021</option>
+            <option value="2022">2022</option>
+            <option value="2023">2023</option>
+            <option value="2024">2024</option>
+            <option value="2025">2025</option>
         </select>
         </p>
       </div>
