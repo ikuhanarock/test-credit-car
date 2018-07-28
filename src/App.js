6e162a6dbd0c10850be47396fcabfa6e62cd7214
Yuta Uchida <rjrhw977@yahoo.co.jp>
2018-07-28 17:55:09 +0900

    Add select item

--- src/App.js
+++ src/App.js
@@ -19,15 +19,22 @@ class App extends Component {
             <option value="1" selected="selected">01</option>
             <option value="2">02</option>
             <option value="3">03</option>
-                      〜 中略 〜
+            <option value="4">04</option>
+            <option value="5">05</option>
+            <option value="6">06</option>
+            <option value="7">07</option>
+            <option value="8">08</option>
+            <option value="9">09</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
             <option value="12">12</option>
           </select>
           <select name="cardExpirationYear">
-            <option value="2015" selected="selected">2015</option>
-            <option value="2016">2016</option>
-            <option value="2017">2017</option>
-                      〜 中略 〜
-            <option value="2035">2035</option>
+            <option value="" selected="selected"></option>
+            <option value="2018">2018</option>
+            <option value="2019">2019</option>
+            <option value="2020">2020</option>
+            <option value="2021">2021</option>
         </select>
         </p>
       </div>
